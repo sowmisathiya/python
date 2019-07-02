3ea25568ba9a670bc7b34191e995b14a0cb688c9
sowmisathiya <51949283+sowmisathiya@users.noreply.github.com>
2019-07-02 11:26:54 +0530

    Update 16.py

--- 16.py
+++ 16.py
@@ -1,5 +1,5 @@
-sowmi1,sa2=map(int,input().split())
-for i in range(sowmi1+1,sa2,1):
+a1,b2=map(int,input().split())
+for i in range(a1+1,b2,1):
   if(i>1):
     for j in range(2,i):
       if(i%j==0):
