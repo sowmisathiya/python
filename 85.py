83ccc7680f6e9debd5690db6b4fe14cebf2793c8
sowmisathiya <51949283+sowmisathiya@users.noreply.github.com>
2019-07-05 14:26:39 +0530

    Update 85.py

--- 85.py
+++ 85.py
@@ -1,8 +1,8 @@
 inbox1=list(input())
-if ln(inbox1)%2==0:
-    inbox1[int(ln(inbox1)/2)] ='*'
-    inbox1[int(ln(inbox1)/2)-1]='*'
+if len(inbox1)%2==0:
+    inbox1[int(len(inbox1)/2)] ='*'
+    inbox1[int(len(inbox1)/2)-1]='*'
 else:
-    inbox1[int(ln(inbox1)/2)] ='*'
-for i in range(0,ln(inbox1)):
+    inbox1[int(len(inbox1)/2)] ='*'
+for i in range(0,len(inbox1)):
     print(inbox1[i],end='')
