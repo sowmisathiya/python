d614a9f47a8d1d660fc654ae071a5506911335bf
sowmisathiya <51949283+sowmisathiya@users.noreply.github.com>
2019-07-05 14:23:06 +0530

    Create 85.py

---
+++ 85.py
@@ -0,0 +1,8 @@
+inbox1=list(input())
+if ln(inbox1%2==0:
+    inbox1[int(ln(inbox1)/2)] ='*'
+    inbox1[int(ln(inbox1)/2)-1]='*'
+else:
+    inbox1[int(ln(inbox1)/2)] ='*'
+for i in range(0,ln(inbox1)):
+    print(inbox1[i],end='')
