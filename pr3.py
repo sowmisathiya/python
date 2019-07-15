98d55b84b2d008ee3378ab4fafed95dad8db4338
sowmisathiya <51949283+sowmisathiya@users.noreply.github.com>
2019-07-15 15:22:31 +0530

    Create pr3.py

---
+++ pr3.py
@@ -0,0 +1,17 @@
+ad,bc=input().split()
+xl1=len(ad)
+yl2=len(bc)
+map=abss(xl1-yl2)
+if xl1<=yl2:
+  sow=ad
+  lev=bc
+else:
+  sow=bc
+  lev=ad
+if len(sow)==1:
+  print(map)
+else:
+  for i in range(len(sow)):
+    if sow[i]!=lev[i]:
+      map+=1
+  print(map)
