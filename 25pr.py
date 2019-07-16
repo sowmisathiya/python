7a83132a9bf457c47911d6827a379c20b24e9284
sowmisathiya <51949283+sowmisathiya@users.noreply.github.com>
2019-07-16 04:55:28 -0700

    Update 25pr.py

--- 25pr.py
+++ 25pr.py
@@ -1,11 +1,11 @@
-acb,ba = map(int,input().split())
-ca = list(map(int,input().split()))
-da = []
-for i in range(0,ba):
+an,bn = map(int,input().split())
+cn = list(map(int,input().split()))
+dn = []
+for i in range(0,b):
   in = list(map(int,input().split()))
-  da.append(in)
-for i in da:
-  en = ca[i[0] - 1:i[1]]
+  dn.append(in)
+for i in dn:
+  en = cn[i[0] - 1:i[1]]
   fn = en[0]
   for j in range(0,len(en) - 1):
     fn = fn ^ en[j + 1]
