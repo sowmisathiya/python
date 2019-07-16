96f79790d2c6b8c09bc846bbecfbe0ee787ce5ed
sowmisathiya <51949283+sowmisathiya@users.noreply.github.com>
2019-07-16 04:37:55 -0700

    Update pr22.py

--- pr22.py
+++ pr22.py
@@ -2,4 +2,4 @@ man,ran=list(map(int,input().split()))
 ls1=list(map(int,input().split()))
 for i in range(ran):
   u21,v31=list(map(int,input().split()))
-  print(sum(lis1[u21-1:v31]))
+  print(sum(ls1[u21-1:v31]))
