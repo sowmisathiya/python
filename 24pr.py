68212ce91c18f46c8fcab2451a4f547350f09873
sowmisathiya <51949283+sowmisathiya@users.noreply.github.com>
2019-07-16 04:47:03 -0700

    Update 24pr.py

--- 24pr.py
+++ 24pr.py
@@ -1,6 +1,6 @@
 mat,rat = input().split()
-mat,rat = int(mt), int(rt)
-Lat1 = [ int(x) for x in input().split()]
+mat,rat = int(mat), int(rat)
+Lat1 = [ int(xy) for xy in input().split()]
 for i in range(0,rat) :
     aat1,bat1 = input().split()
     aat1,bat1 = int(aat1), int(bat1)
