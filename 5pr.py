4d02d67bd1fd294be79b521b93e94f307546ee40
sowmisathiya <51949283+sowmisathiya@users.noreply.github.com>
2019-07-18 16:28:48 +0530

    Rename pr5.py to 5pr.py

---
+++ 5pr.py
@@ -0,0 +1,13 @@
+def count_1(n) :
+    sss1 = bin(n)[2:]
+    kss = sss1.count('1')
+    return kss
+no = int(input())
+Lo = [ int(x) for x in input().split()]
+Los2 = []
+for i in range(0,no) :
+    Los2.append((count_1(L[i]),L[i]))
+Lo3 = sorted(Los2, key=lambda x : (x[0],x[1]),reverse=True)
+Lo4 = [x[1] for x in Lo3 ]
+for i in range(0,len(Lo4)) :
+    print(Lo4[i])
