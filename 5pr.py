850fe1e1f26dcd0caaa3e37a633a9f928741ccc1
sowmisathiya <51949283+sowmisathiya@users.noreply.github.com>
2019-07-18 16:30:08 +0530

    Update 5pr.py

--- 5pr.py
+++ 5pr.py
@@ -6,7 +6,7 @@ no = int(input())
 Lo = [ int(x) for x in input().split()]
 Los2 = []
 for i in range(0,no) :
-    Los2.append((count_1(L[i]),L[i]))
+    Los2.append((count_1(L[i]),Lo[i]))
 Lo3 = sorted(Los2, key=lambda x : (x[0],x[1]),reverse=True)
 Lo4 = [x[1] for x in Lo3 ]
 for i in range(0,len(Lo4)) :
