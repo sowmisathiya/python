54c1d641c21068e65c89b7768db4a11731f79b4a
sowmisathiya <51949283+sowmisathiya@users.noreply.github.com>
2019-07-20 01:50:49 -0700

    Create 27pr.py

---
+++ 27pr.py
@@ -0,0 +1,16 @@
+np=input()
+a=list(map(int,npi.split()))
+k=a[1]
+ha=input()
+flag=0
+sa=list(map(int,ha.split()))
+for i in range(0,len(sa)-1):
+	for j in range(i+1,len(sa)):
+		if sa[i]+sa[j]==k:
+			print("yes")
+			flag=1
+			break
+	if flag==1:
+		break
+if flag!=1:
+	print("no")
