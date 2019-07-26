7b1a4f682362a5adb4f2b7e2e69ea3c0585a2156
sowmisathiya <51949283+sowmisathiya@users.noreply.github.com>
2019-07-26 14:54:49 +0530

    Create p22.py

---
+++ p22.py
@@ -0,0 +1,11 @@
+sow,sa=map(int,input().split())
+lt=list(map(int,input().split()))
+lw=[]
+for i in range(0,sa):
+     sh,she1=ap(int,input().split())
+     lw.append([sh,she1)
+for i n range(sa):
+     as=lw[i][0]
+     up=lw[i[1]
+     yw=sum(lost[as-1:up])
+     print(yw)
