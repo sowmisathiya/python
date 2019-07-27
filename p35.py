969a6c35e4c1d4193de384b506a059bd419b5dc1
sowmisathiya <51949283+sowmisathiya@users.noreply.github.com>
2019-07-27 14:23:49 +0530

    Update p35.py

--- p35.py
+++ p35.py
@@ -4,7 +4,7 @@ max=0
 i1=0
 while(i1<len(leel1)-1):
     count1=0
-    while(i1<len(level1)-1 and leel1[i1]<level1[i1+1]):
+    while(i1<len(level1)-1 and leel1[i1]<leel1[i1+1]):
         count1+=1
         i1+=1
     if(count1>max):
