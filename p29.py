e336fc8fc64ef908e9e037e0430d745cefa63d0e
sowmisathiya <51949283+sowmisathiya@users.noreply.github.com>
2019-07-27 09:28:55 +0530

    Create p29.py

---
+++ p29.py
@@ -0,0 +1,11 @@
+cou1=int(input())
+arr=[]
+ss1=[]
+for i in (cou1):
+  arr.append(list(map(int,input().split())))
+for l in arr:
+  for n in l:
+    ss1.append(n)
+ ss1.sort()
+ for i in ss1:
+  print(i,end=' ')
