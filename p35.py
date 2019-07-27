b41739c5fe29d5bd1ef5b3f69bd4de0ef355fab0
sowmisathiya <51949283+sowmisathiya@users.noreply.github.com>
2019-07-27 14:22:32 +0530

    Create p35.py

---
+++ p35.py
@@ -0,0 +1,13 @@
+nr1=input()
+leel1=list(map(int,input().split()))
+max=0
+i1=0
+while(i1<len(leel1)-1):
+    count1=0
+    while(i1<len(level1)-1 and leel1[i1]<level1[i1+1]):
+        count1+=1
+        i1+=1
+    if(count1>max):
+        max=count1
+    i1+=1
+print(max+1)
